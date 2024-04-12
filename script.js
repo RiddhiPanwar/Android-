3444d68218ceee3d9a8e52e352e34743b8f86f9f
Riddhi Panwar <132136373+RiddhiPanwar@users.noreply.github.com>
2024-04-12 16:45:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function android4(){
+    window.location.href = 'android2.html#';
+}
+function android2(){
+    window.location.href = 'android4.html#';
+}
+function androidCreate(){
+    window.location.href = 'android6.html#';
+}
+function login(){
+    window.location.href = 'android5.html#'
+}
+function createAccount(){
+    window.location.href = 'android4.html#'
+}
+function signIn(){
+    window.location.href = 'android6.html#'
+}
+function confirm(){
+    window.location.href = 'android8.html#'
+}
+function lastPage(){
+    window.location.href = 'android9.html#'
+}
+function returnPage(){
+    window.location.href = 'android8.html#'
+}
